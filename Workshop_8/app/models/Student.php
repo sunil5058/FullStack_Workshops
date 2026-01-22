2e5272326ca5a4cd0f4bd8e3914d07f2d1af96fe
sunil-shrestha <sunilshrestha2061@gmail.com>
2026-01-22 22:36:12 +0545

    workshop 8 initialized

---
+++ Workshop_8/app/models/Student.php
@@ -0,0 +1,23 @@
+<?php 
+class Student {
+    public PDO $pdo;
+    public  function __construct(PDO $pdo) {
+       
+    }
+
+    public function all (){
+
+    }
+    public function find (int $id){
+
+    }
+    public function create (string $name, string $email, string $course){
+
+    }
+    public function update (string $name, string $email, string $course, int $id){
+
+    }
+    public
+    
+}
+?>
---
+++ Workshop_8/db.php
@@ -0,0 +1,21 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'herald_db';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$server;dbname=$database;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
+}
+?>
